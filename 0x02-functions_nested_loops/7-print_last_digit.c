14308a55d0677a36fd000f352385edc256656c1f
OluJakes <lummiee@gmail.com>
2023-06-20 12:31:57 -0700

    7-print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,14 +10,11 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	/* Get the absolute value of n */
 	if (n < 0)
 		n = -n;
-
-	/* Extract the last digit */
+	
 	last_digit = n % 10;
-
-	/* Print the last digit */
+	
 	_putchar(last_digit + '0');
 
 	return (last_digit);
