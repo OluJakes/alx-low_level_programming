2b40dae2939bcdcd606f05a0387bd4e846a28219
OluJakes <lummiee@gmail.com>
2023-06-20 09:57:45 -0700

    isaplha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,15 +3,15 @@
 /**
  * _isapha - checks if a character is an alphabetic characyer
  * @c: The character to be checked
- * 
+ *
  * Return: 1 if c is a letter, lower or uppercase; 0 otherwise
  */
 int _isalpha(int c)
 {
   if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
     {
-      return (1);
-    }
+      return (1)
+	}
   else
     {
       return (0);
