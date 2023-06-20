b957c80071ad548c1af83f6d67ef6f9cc5b15d6a
OluJakes <lummiee@gmail.com>
2023-06-20 10:58:13 -0700

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,7 +11,7 @@
  */
 int print_sign(int n)
 {
-	if (n>0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
@@ -21,7 +21,7 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else 
+	else
 	{
 		_putchar('-');
 		return (-1);
