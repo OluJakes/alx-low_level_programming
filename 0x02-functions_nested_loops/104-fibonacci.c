987967f9e0d243908e1ae6fa1638d2bb277bb4ce
OluJakes <lummiee@gmail.com>
2023-06-24 01:38:57 -0700

    104-fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,47 @@
 #include <stdio.h>
 
 /**
- * main - finds and prints the sum of even-valued Fibonacci numbers
- *        that do not exceed 4,000,000
- * Return: 0
+ * main - fibonacci <3
+ *
+ * Purpose - no hardcode
+ *
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	int num1 = 1, num2 = 2, next;
-	int sum = 2; /* Start with 2 as the sum (accounting for the initial even number) */
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1;
+	unsigned long int aft2;
 
-	while (num2 <= 4000000)
+	printf("%lu", bef);
+
+	for (i = 1; i < 91; i++)
 	{
-		next = num1 + num2;
-
-		if (next % 2 == 0)
-			sum += next;
-
-		num1 = num2;
-		num2 = next;
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
 	}
 
-	printf("%d\n", sum);
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
 
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
+	}
+	printf("\n");
 	return (0);
 }
 
