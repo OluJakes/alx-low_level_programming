dd3ecb7c12078f4a423f4bf19f86885db0e00e50
Olumide Solanke <olumide@greyfusion.com.ng>
2023-08-03 13:07:19 +0100

    101-password

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,31 +1,26 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Standard Library Headers */
-#include <stddef.h>
-
-/* Function Prototypes */
-
-/* Converts a binary number to an unsigned int. */
+/* Task 0 - Binary to Unsigned int */
 unsigned int binary_to_uint(const char *b);
 
-/* Prints the binary representation of a number. */
+/* Task 1 - Print Binary */
 void print_binary(unsigned long int n);
 
-/* Returns the value of a bit at a given index. */
+/* Task 2 - Get Bit */
 int get_bit(unsigned long int n, unsigned int index);
 
-/* Sets the value of a bit to 1 at a given index. */
+/* Task 3 - Set Bit */
 int set_bit(unsigned long int *n, unsigned int index);
 
-/* Sets the value of a bit to 0 at a given index. */
+/* Task 4 - Clear Bit */
 int clear_bit(unsigned long int *n, unsigned int index);
 
-/* Returns the number of bits needed to flip to get from one number to another. */
+/* Task 5 - Flip Bits */
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
-/* Provided function for printing characters. */
-int _putchar(char c);
+/* Task 6 - Get Endianness */
+int get_endianness(void);
 
 #endif /* MAIN_H */
 
